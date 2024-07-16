7844668401a258274d9628389605ff620a6180cc
MIHIRPATEL2715 <mihirpqtel@gmail.com>
2024-07-16 21:24:54 +0530

    mihir

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Inputtext from "./components/input";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <center>
+      <div className="main">
+        <Inputtext></Inputtext>
+      </div>
+    </center>
+  );
+}
+
+export default App;
---
+++ src/components/input.jsx
@@ -0,0 +1,28 @@
+function Inputtext() {
+  return (
+    <>
+      <h1 class="display-5">Ask to priti</h1>
+      <div class="input-group textarea">
+        <textarea
+          class="form-control textarea"
+          placeholder="Don't think just ask...."
+          aria-label="With textarea"
+        ></textarea>
+      </div>
+
+      <div class="input-group mb-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Enter Text"
+          aria-label="Enter Text"
+          aria-describedby="button-addon2"
+        />
+        <button class="btn btn-primary" type="button" id="button-addon2">
+          Submit
+        </button>
+      </div>
+    </>
+  );
+}
+export default Inputtext;
